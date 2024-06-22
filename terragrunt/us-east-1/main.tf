52f27a528c0363ed86df482848ff9817f3a34c0a
mr-kaveh <mr.hdavoodi@gmail.com>
2024-06-22 18:06:09 +0200

    Update and rename terragrunt.hcl to main.tf

--- terragrunt/us-east-1/main.tf
+++ terragrunt/us-east-1/main.tf
@@ -1,17 +1,18 @@
+// us-east-1/main.tf
 terraform {
-  source = "../terraform/aurora"
+  source = "../../terraform/aurora"
 }
 
 inputs = {
-  region                = "eu-east-1"
+  region                = "us-east-1"
   cluster_identifier    = "my-global-db"
   master_username       = "admin"
   master_password       = "password123"
   database_name         = "mydatabase"
-  availability_zones    = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+  availability_zones    = ["us-east-1a", "us-east-1b", "us-east-1c"]
   instance_count        = 2
   instance_class        = "db.r5.large"
   db_subnet_group_name  = "aurora-subnet-group"
-  subnet_ids            = ["subnet-98765432", "subnet-87654321", "subnet-76543210"]
-  vpc_id                = "vpc-87654321"
+  subnet_ids            = ["subnet-12345678", "subnet-23456789", "subnet-34567890"]
+  vpc_id                = "vpc-12345678"
 }
